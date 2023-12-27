7488acc5a32c7c1017270715af6e38c03749f760
animesh ojha <133531300+animeshojha@users.noreply.github.com>
2023-12-27 20:04:14 +0000

    datatypes

---
+++ Basic/datatypes.js
@@ -0,0 +1,12 @@
+console.log("in this we will leran about datatypes");
+
+let name="animesh"
+console.log(typeof name);
+let age=12;
+console.log(typeof (age));
+let ans=true;
+console.log(typeof ans);
+// types of null will be a object 
+console.log(typeof null);
+// type of undefined will be undefined
+console.log(typeof undefined);
