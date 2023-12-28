914405b12dd07301f4bb218553c32a25b550c74e
animesh ojha <133531300+animeshojha@users.noreply.github.com>
2023-12-28 17:08:57 +0000

    datatype-conversion

--- Basic/datatype-conversion.js
+++ Basic/datatype-conversion.js
@@ -1,2 +1,28 @@
 let score = 77
-console.log(typeof score);
+//console.log(typeof score);
+
+let string = "77"
+//console.log(typeof string);
+//let value=Number(string)
+//console.log(value);
+//console.log(typeof value);
+// in conversion to boolean ........
+
+// let number=undefined
+// let ans=Boolean(number)
+// console.log(ans);
+// console.log(typeof ans);
+
+// in conversion to number.........
+
+// let value=undefined
+// let ans=Number(value)
+// console.log(ans);
+// console.log(typeof ans);
+
+
+// in conversion to string ............
+let value =undefined
+let ans=String(value)
+console.log(typeof ans);
+console.log(ans);
