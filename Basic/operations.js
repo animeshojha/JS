b738b22913627080f7428850d84b7a17055cc450
animesh ojha <133531300+animeshojha@users.noreply.github.com>
2023-12-28 18:15:49 +0000

    operations

---
+++ Basic/operations.js
@@ -0,0 +1,28 @@
+console.log("hello");
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**4);
+// console.log(2/2);
+// console.log(2%5);
+
+// let str1="Hello"
+// let str2=" Animesh"
+// console.log(str1+str2);
+
+// console.log("1"+ 2);
+// console.log(1+ "2");
+// console.log("1" + 2 +2);
+// console.log(1+2+"2");
+// console.log(1+"1"+1);
+// console.log(+false);
+
+// prefix and postfix
+
+let a=4;
+console.log(a++)
+console.log(++a);
+let b = a++ + ++a 
+
+console.log(b);
