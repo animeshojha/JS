4ddeb0dba6cebd26c39e9b97d8c3294aeee63c72
animesh ojha <133531300+animeshojha@users.noreply.github.com>
2024-01-14 23:02:36 +0000

    variables

--- Basic/variables.js
+++ Basic/variables.js
@@ -1,9 +1,9 @@
 console.log("in this we will learn about variables")
 // const can not be re-assigned and redeclare and it has local scope
 const name="animesh"
+
 // let can be re-assigned but can not be re-declare and it has local scope
 let email="animeshojha44@gmail.com"
-
 email="deepuojha969@gmail.com"
 // var can be re-assigned and re-declare and it has gloabl scope
 var password="1234"
