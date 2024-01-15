773e98c744034aaade64bf58a4636f6e70ebf0d0
animesh ojha <133531300+animeshojha@users.noreply.github.com>
2024-01-15 00:55:18 +0000

    datatpe-summary

--- Basic/datatypes.js
+++ Basic/datatypes.js
@@ -11,3 +11,44 @@ console.log(typeof ans);
 console.log(typeof null);
 // type of undefined will be undefined
 console.log(typeof undefined);
+
+// summary/.............................................
+
+ /*
+ there are two types  of datatypes in js
+ 1. premitive -> number , boolean, string, null , undefined , bigint , symbol ( a copy will be made for changes)
+     ->> also known as value type
+ 2. non-premitive -> array , object , function (changes will be in the original)
+     ->> also known as reference type
+
+  js is dynamically typed languages where the interpreter assigns variable a type at the runtime according to the
+  variables value at that time   
+ */
+
+// symbol used for uniqnesss
+
+let a=Symbol("123");
+let b=Symbol("123");
+console.log(a==b);
+console.log(a===b);
+console.log(typeof a);
+
+// array
+const name1=["animesh" , "deepu", "ABCDE"];
+// object
+let myobj={
+    name:"animesh",
+    age:12,
+    college:"itm"
+};
+
+const myfunction=function(){
+    console.log("i am function");
+}
+
+console.log(typeof name1);
+console.log(typeof myobj);
+console.log(typeof myfunction);
+
+let bigint=25423432n;
+console.log(typeof bigint);
