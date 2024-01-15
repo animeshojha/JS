cff33370e55f823cdefd54935e7d36f1c2fabf8f
animesh ojha <133531300+animeshojha@users.noreply.github.com>
2024-01-15 23:19:22 +0000

    array1

--- Basic1/array.js
+++ Basic1/array.js
@@ -11,10 +11,32 @@ number.push(62);
 number.pop();
 number.unshift(0);
 number.shift()
-console.log(number);
+// console.log(number);
 // console.log(number.includes(12));
 // console.log(number.sort())
-console.log(number.slice(1,4));
+// console.log(number.slice(1,4));
 let n=number.join()  // join converts the array into string
 // console.log(n); 
-console.log(number.splice(0 , 2))
+
+const myarr=[0,1,2,3,4,5];
+console.log("A ", myarr);
+
+// let mya1=myarr.slice(1,3);
+// console.log(mya1);
+// console.log("original array after slice operation");
+// console.log("B ",myarr);
+// let mya2=myarr.splice(1,4);
+// console.log(mya2);
+// console.log("original array after splice operation");
+// console.log(myarr);
+
+// let m=[1,2,3,4,5,6,7,8,9];
+// console.log(m);
+// let o=m.slice(1,4);
+// console.log(o);
+// console.log(m);
+// let p=m.splice(1,3);
+// console.log(p);
+// console.log(m);
+
+
