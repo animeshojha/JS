287876431a81f82cd6fe56523b9a93fb83791f3c
animesh ojha <133531300+animeshojha@users.noreply.github.com>
2024-01-15 00:17:54 +0000

    comparison

---
+++ Basic/comparison.js
@@ -0,0 +1,13 @@
+console.log("1"==1);
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+console.log(undefined>0);
+console.log(undefined==0);
+console.log(undefined>=0);
+console.log(undefined>1);
+console.log(undefined==1);
+
+console.log(null===0);
+console.log("1"==1);
+console.log("2"===2);
