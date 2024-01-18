356be351a1e7d1539c9192acbe8b8b8af5171d1c
animesh ojha <133531300+animeshojha@users.noreply.github.com>
2024-01-18 12:38:43 +0000

    while - do while

---
+++ Iterations/loops1.js
@@ -0,0 +1,19 @@
+let a=0;
+while(a<=10){
+    //console.log(`value of a is ${a}`);
+    a=a+2;
+}
+
+//console.log(`printing array` );
+let myarray=[1,2,3,4,5];
+let arr=0;
+while(arr<myarray.length){
+    //console.log(myarray[arr]);
+    arr++;
+}
+
+let score = 11
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
