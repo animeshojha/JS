ab34611ece1336cc9bbb59e80adec253c454b659
animesh ojha <133531300+animeshojha@users.noreply.github.com>
2024-01-18 10:02:07 +0000

    if-else

---
+++ Control_flow/first.js
@@ -0,0 +1,12 @@
+let ans=2;
+if(ans){
+    console.log(ans);
+}
+
+const score=12;
+if(score>10){
+    var name="fly";
+    console.log(score);
+    console.log(`lets ${name}`);
+}
+console.log(`lets ${name}`);
