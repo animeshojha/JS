3953366139bb8c5b46249be8f81f1a43748bbeb3
animesh ojha <133531300+animeshojha@users.noreply.github.com>
2024-01-18 10:17:17 +0000

    switch

---
+++ Control_flow/switch.js
@@ -0,0 +1,18 @@
+const month=3;
+switch(month){
+    case 1:
+        console.log('january');
+        break;
+
+    case 2:
+        console.log('febuary');  
+        break;
+        
+    case 3:
+        console.log('march');
+        break;
+
+    default:
+        console.log('error');
+        break;
+}
